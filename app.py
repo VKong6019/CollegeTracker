06016b31ba13af67697de5f95d8ecce99f0f0e68
Nicole Danuwidjaja <danuwidjajanicole@gmail.com>
2019-11-14 17:05:14 -0500

    front end

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Home"
+    return render_template()
 
 if __name__ == "__main__":
     app.run()
